94b50d75d08e4d6d47dbcd095ea813d262c9bad9
mohamedyasser25 <my1844401@gmail.com>
2023-02-04 00:29:54 +0200

    first commit

---
+++ lib/data/models/applicant.dart
@@ -0,0 +1,13 @@
+class Applicant {
+  final int ID;
+  final String name;
+  final String email;
+  final int phone;
+  final String specialization;
+  const Applicant(
+      {required this.ID,
+      required this.name,
+      required this.email,
+      required this.phone,
+      required this.specialization});
+}
---
+++ lib/login.dart
@@ -0,0 +1,95 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  var formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email Address',
+                    prefixIcon: Icon(
+                      Icons.email,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      // print(emailController.text);
+                      // print(passwordController.text);
+                      var result = await FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passwordController.text);
+                      if (result != null) {
+                        print('welcome');
+                      } else {
+                        print('null');
+                      }
+                    },
+                    child: const Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
